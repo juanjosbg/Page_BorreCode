c070211bc556a36c8ceafdfbac6710cc036932a6
juanjosbg <juanjoseborrero95@gmail.com>
2021-05-03 17:19:44 -0500

    added node

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = ('path');
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports={
+    mode: 'development',
+    entry:'/js/index.js',
+    output:{
+        path: path.join(__dirname, 'dist'),
+        filename: bungle.js
+    },
+    module:{
+        rules: [
+            {
+                test:/\.css$/,
+                use:['sytle-loader', 'css-loader']
+            }
+        ]
+    },
+    plugin:{
+
+    }
+};
