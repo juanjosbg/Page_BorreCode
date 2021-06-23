e7f138b0a9eaa7d155accb3bd78fd5ea0b4256d3
juanjosbg <juanjoseborrero95@gmail.com>
2021-06-23 15:47:47 -0500

    chages on the page

--- contend/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var webpack = requiere('webpack');
-var webpackDevMiddleware = require('webpack-dev-midd');
-var webpackConfig = require('/webpack.config');
-
-var app = express();
-app.set('port',(process.env.PORT || 3000));
-app.set('/static',express.static('dist'));
-app.set(webpackDevMiddleware(webpack(webpackConfig)));
